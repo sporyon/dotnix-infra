7601b355455d4b9910b7b378adec0e83a8b10a01
ra33it0 <sebastian@sporyon.io>
2024-07-22 04:04:57 +0200

    Init dotnix-infra

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.disko.url = "github:nix-community/disko";
+  inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { nixpkgs, disko, ... }:
+    {
+      # tested with 2GB/2CPU droplet, 1GB droplets do not have enough RAM for kexec
+      nixosConfigurations.dotnix-infra = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./configuration.nix
+        ];
+      };
+    };
+}
