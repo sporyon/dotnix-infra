51c44c65eebd2cd1e9481cddf03f3529e43c0337
ra33it0 <sebastian@sporyon.io>
2024-07-22 04:14:54 +0200

    Flake.nix: add dotnix-core

--- flake.nix
+++ flake.nix
@@ -2,12 +2,13 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.dotnix.url = "github:sporyon/dotnix-core";
 
-  outputs = { nixpkgs, disko, ... }:
+  outputs = { nixpkgs, disko, dotnix-core, ... }:
     {
       # tested with 2GB/2CPU droplet, 1GB droplets do not have enough RAM for kexec
       nixosConfigurations.dotnix-infra = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
+        system = "x86_64-linux";
         modules = [
           disko.nixosModules.disko
           ./configuration.nix
